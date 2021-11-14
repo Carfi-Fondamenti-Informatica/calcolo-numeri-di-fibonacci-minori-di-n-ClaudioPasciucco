458a2641f48a82853f52ae259c5a1d0a05420a35
ClaudioPasciucco <91784159+ClaudioPasciucco@users.noreply.github.com>
2021-11-14 17:10:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
    int n=0, x1=0, x2=1, x3=x1+x2;
    cin >> n;
-   if (!(n==0)){
       cout << x2 << endl;
       for (int i=2; i<=n; i++){
          if (x3<=n){
@@ -13,7 +12,6 @@ int main() {
             x2 = x3;
             x3 = x1 + x2;
           }
-       }
-   }  
+       } 
    return 0;
 }
